abee624dda7115d01d4a27582ace5cda09f126a4
Xia Zhenchao <xiazhenchao96@gmail.com>
2022-12-07 18:25:13 -0500

    finish search

--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect } from 'react'
 import { GlobalStoreContext } from '../store'
 import ListCard from './ListCard.js'
 import MUIDeleteModal from './MUIDeleteModal'
-
+import {useState } from 'react';
 import AddIcon from '@mui/icons-material/Add';
 import Fab from '@mui/material/Fab'
 import List from '@mui/material/List';
@@ -15,7 +15,7 @@ import SortIcon from '@mui/icons-material/Sort';
 import { Box } from '@mui/system';
 import Grid from '@mui/material/Grid';
 import YouTubePlayerExample from './PlaylisterYouTubePlayer';
-import { Tab, Tabs } from '@mui/material';
+import { IconButton, Tab, Tabs } from '@mui/material';
 import YouTubePlayer from './PlaylisterYouTubePlayer';
 /*
     This React component lists all the top5 lists in the UI.
@@ -27,13 +27,15 @@ const HomeScreen = () => {
     useEffect(() => {
         store.loadIdNamePairs();
     }, []);
+    // let PageState = "HOME";
+    const [PageState, setPageState] = useState("HOME");
+    const [KeyWord,setKeyWord] = useState("");
 
     function handleCreateNewList() {
         store.createNewList();
     }
     
     function handleCommentViewButton(){
-       
        store.YoutubeStateComment()
        console.log("Comment")
     }
@@ -41,10 +43,41 @@ const HomeScreen = () => {
         store.YoutubeStatePlayer();
     }
 
+    function handleHomeScreenView(){
+        setPageState("HOME")
+        setKeyWord("")
+        // PageState = "HOME"
+    }
+
+    function handleAllListScreenView(){
+        // PageState = "GROUP"
+        setPageState("GROUP")
+        setKeyWord("")
+    }
+
+    function handleUserListScreenView(){
+        // PageState = "USER"
+        setPageState("USER")
+        setKeyWord("")
+    }
+
+    function handleSearchInput(event){
+        if(event.keyCode == 13){
+            let temp = event.target.value
+            setKeyWord(temp)
+            console.log("The key word is: "+ KeyWord)
+        }
+    }
+
+
     let value = 0;
     let listCard = "";
     if (store) {
-        listCard = 
+       if(PageState == 'HOME')
+       {
+        if(KeyWord == "")
+        {
+            listCard = 
             <List sx={{ width: '90%', left: '5%', bgcolor: '#6495ED' }}>
             {
                 store.idNamePairs.map((pair) => (
@@ -58,16 +91,100 @@ const HomeScreen = () => {
                 ))
             }
             </List>;
+        }
+        else{
+            listCard = 
+            <List sx={{ width: '90%', left: '5%', bgcolor: '#6495ED' }}>
+            {
+                store.idNamePairs.filter(pair => pair.name == KeyWord).map((pair) => (
+                   <div>
+                     <ListCard
+                        key={pair._id}
+                        idNamePair={pair}
+                        selected={false}
+                    />
+                   </div>
+                ))
+            }
+            </List>;
+        }
+       }
+       else if(PageState != "HOME"){
+         if(KeyWord == "")
+         {
+            listCard = 
+            <List sx={{ width: '90%', left: '5%', bgcolor: '#6495ED' }}>
+            {
+                store.idNamePairs.filter(pair => typeof pair.publishDate !== 'undefined').map((pair) => (
+                   <div>
+                     <ListCard
+                        key={pair._id}
+                        idNamePair={pair}
+                        selected={false}
+                    />
+                   </div>
+                ))
+            }
+            </List>;
+         }
+         else{
+            if(PageState == "USER"){
+                listCard = 
+                <List sx={{ width: '90%', left: '5%', bgcolor: '#6495ED' }}>
+                {
+                    store.idNamePairs.filter(pair => typeof pair.publishDate !== 'undefined' && pair.name == KeyWord).map((pair) => (
+                       <div>
+                         <ListCard
+                            key={pair._id}
+                            idNamePair={pair}
+                            selected={false}
+                        />
+                       </div>
+                    ))
+                }
+                </List>;
+            }
+
+            if(PageState == "GROUP"){
+                listCard = 
+                <List sx={{ width: '90%', left: '5%', bgcolor: '#6495ED' }}>
+                {
+                    store.idNamePairs.filter(pair => typeof pair.publishDate !== 'undefined' && pair.name == KeyWord).map((pair) => (
+                       <div>
+                         <ListCard
+                            key={pair._id}
+                            idNamePair={pair}
+                            selected={false}
+                        />
+                       </div>
+                    ))
+                }
+                </List>;
+            }
+
+         }
+       }
     }
+
+
+
     return (
         <div id="playlist-selector">
             <div id="list-selector-heading">
             </div>
              <div style={{fontSize: '30pt'}}>
-                <HomeIcon style={{fontSize: '45pt'}}/>
-                <PersonIcon style={{fontSize: '45pt'}}/>
-                <GroupsIcon style={{fontSize: '45pt'}}/>
-                <TextField id="filled-basic" label="Search" variant="filled" style={{left:'10%',width:'800px', backgroundColor:'#FFFFFF'}}/>
+                <IconButton onClick={handleHomeScreenView}>
+                    <HomeIcon style={{fontSize: '45pt'}}  />
+                </IconButton>
+                
+                <IconButton onClick={handleUserListScreenView}>
+                    <PersonIcon style={{fontSize: '45pt'}}/>
+                </IconButton>
+                <IconButton onClick={handleAllListScreenView}>
+                    <GroupsIcon style={{fontSize: '45pt'}}/>
+                </IconButton>
+                <TextField id="filled-basic" label="Search" variant="filled" style={{left:'10%',width:'800px', backgroundColor:'#FFFFFF'}}
+                onKeyDown = {handleSearchInput}/>
                 <span style={{marginLeft: '20%',fontSize:'40pt'}}>SORT BY</span>
                 <SortIcon style={{fontSize: '45pt',right:'40%'}}/>
             </div>
@@ -85,7 +202,7 @@ const HomeScreen = () => {
                     <Grid item xs={5} >
                    <div>
                     <button style={{fontSize: '30px', width:'150px',backgroundColor:'#A9A9A9'}} onClick ={handleYoutuplayerViewButton}>
-                            Player
+                            Player 
                         </button>
                         <button style={{fontSize: '30px', width:'150px'}} onClick= {handleCommentViewButton}>
                             Comment
